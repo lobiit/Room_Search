948c76c5e62ea7c3e701eccb79af0827175afa44
Tumbo Lobi John <johnlobiit@gmail.com>
2022-03-28 11:04:07 +0300

    customized the admin area

--- room/settings.py
+++ room/settings.py
@@ -126,7 +126,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'room/static')
 ]
-
+# Media folder settings
+MEDIA_ROOT = os.path.join(BASE_DIR, ' media')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
