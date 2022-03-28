3fb1805590f07abfb085ea4e35117f4b57407d66
Tumbo Lobi John <johnlobiit@gmail.com>
2022-03-28 11:51:34 +0300

    Able to pull data from listings model and display listings in template

--- realtors/admin.py
+++ realtors/admin.py
@@ -8,7 +8,7 @@ from .models import Realtor
 class RealtorAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'email', 'hire_date')
     list_display_links = ('id', 'name')
-    search_fields = 'name'
+    search_fields = 'name',
 
 
-admin.site.register(Realtor)
+admin.site.register(Realtor, RealtorAdmin)
--- room/settings.py
+++ room/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'pages',
     'listings',
     'realtors',
+    'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
