2a844b622d8ecf97b2f123a36992b385c17149e5
Tumbo Lobi John <johnlobiit@gmail.com>
2022-03-28 18:33:29 +0300

    search form choices, filtering and preserving form input

---
+++ listings/choices.py
@@ -0,0 +1,18 @@
+location_choices = {
+    'Mabs': 'Mabs',
+    'Stage': 'Stage',
+    'Chebarus': 'Chebarus',
+}
+
+price_choices = {
+    '1000': 'KSH. 1000',
+    '2000': 'KSH.2000',
+    '3000': 'KSH.3000',
+    '4000': 'KSH.4000',
+    '5000': 'KSH.5000',
+    '6000': 'KSH.6000',
+    '7000': 'KSH.7000',
+    '8000': 'KSH.8000',
+    '9000': 'KSH.9000',
+    '10000': 'KSH.10000',
+}
--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Listing
+from listings.choices import location_choices, price_choices
 
 
 # Create your views here.
@@ -26,4 +27,33 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+    # Keyword
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    # title
+    if 'title' in request.GET:
+        title = request.GET['title']
+        if title:
+            queryset_list = queryset_list.filter(title__iexact=title)
+        # Location
+    if 'location' in request.GET:
+        location = request.GET['location']
+        if location:
+            queryset_list = queryset_list.filter(location__iexact=location)
+        # Keywors
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
+    context = {
+        'location_choices': location_choices,
+        'price_choices': price_choices,
+        'listings': queryset_list,
+        'values': request.GET
+    }
+    return render(request, 'listings/search.html', context)
